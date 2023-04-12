d45c695262ece1e5ba4e367b9bd0af627da1f4e1
jaishana25 <96332944+jaishana25@users.noreply.github.com>
2023-04-12 15:00:58 +0530

    property client again

--- src/PropertyForm.js
+++ src/PropertyForm.js
@@ -13,55 +13,67 @@ function PropertyForm() {
         <input type='text3' placeholder='Enter Property Number' name='property'>
 
         </input>
+        
     </div>
     <div>
-        <input type='text3' placeholder='Type' name='type'>
-            
-        </input>
-
-        <input type='text2' placeholder='Rooms' name='Rooms'>
+    <input type='text3' placeholder='Type' name='type'>
             
             </input>
-    </div>
-    <div>
-        <input type='text3' placeholder='Rent' name='rent'>
+
+        <input type='text2' placeholder='Rooms' name='rooms'>
+            
+            </input>
+            <input type='text3' placeholder='Rent' name='rent'>
             
         </input>
+    </div>
+    <div>
+        <input type='text' placeholder='Enter Property Address' name='address'>
+            
+            </input>
+    
 
        
     </div>
     <div>
-        <input type='text'  placeholder='Address' name='salary'>
-
-        </input>
-        <input type='text'  placeholder='Address' name='salary'>
-
-</input>
-    </div>
-    <div>
-        <input type='text3' placeholder='Branch Number' name='brno'>
-
-        </input>
-        <input type='text2' placeholder='Telephone Number' name='number'>
-
-        </input>
-        <input type='text' placeholder='Branch Address' name='address'>
+        <input type='text3'  placeholder='Managed by ' name='staff'>
 
         </input>
         
     </div>
     <div>
-        <input type='text2'  placeholder='Supervisor Name' name='supervisor'>
+        <input type='text3' placeholder='Owner Number' name='Ono'>
 
         </input>
-        <input type='text2'  placeholder='Manager Start Date' name='supervisor'>
+        <input type='text2' placeholder='Business Name' name='bname'>
 
         </input>
+        
+        
     </div>
     <div>
-        <input type='text2'  placeholder='Manager Bonus' name='bonus'>
+    <input type='text' placeholder='Address' name='address'>
+
+</input>
+    </div>
+    <div>
+        <input type='text3'  placeholder='Telephone Number' name='number'>
 
         </input>
+        
+    </div>
+    <div>
+        <input type='text2'  placeholder='Type of business' name='business'>
+
+        </input>
+        <input type='text3'  placeholder='Contact Name' name='cname'>
+
+</input>
+    </div>
+    <div>
+    <input type='text'  placeholder='Registered at branch' name='branch'>
+
+</input>
     </div>
     <div>
         <button type='submit'>Submit </button>
